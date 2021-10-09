1a43bfa5d19eeb7e5128c66486e0e7a56b60d4a1
Admin <admin@Mikes-MacBook-Air.local>
2021-10-08 22:33:40 -0400

    updated platformer a little further almost complete just missing a few steps to clean it up

--- games/src/games/platformer.py
+++ games/src/games/platformer.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import random
+import time
 
 from pygame.locals import *
 
@@ -22,13 +23,14 @@ pygame.display.set_caption("Game")
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.surf = pygame.Surface((30, 30))
-        self.surf.fill((128,255,40))
-        self.rect = self.surf.get_rect(center = (10, 420))
+        self.surf = pygame.image.load("snowman.png")
+        self.rect = self.surf.get_rect()
 
-        self.pos = vec((10, 385))
+        self.pos = vec((10, 360))
         self.vel = vec(0, 0)
         self.acc = vec(0, 0)
+        self.jumping = False
+        self.score = 0
 
     def move(self):
         self.acc = vec(0, 0.5)
@@ -52,34 +54,97 @@ class Player(pygame.sprite.Sprite):
     
     def jump(self):
         hits = pygame.sprite.spritecollide(self, platforms, False)
-        if hits: 
+        if hits and not self.jumping: 
+            self.jumping = True
             self.vel.y = -15
+    
+    def cancel_jump(self):
+        if self.jumping:
+            if self.vel.y < -3:
+                self.vel.y = -3
 
     def update(self):
         hits = pygame.sprite.spritecollide(P1, platforms, False)
-        if P1.vel.y > 0:
+        if self.vel.y > 0:
             if hits:
-                self.vel.y = 0
-                self.pos.y = hits[0].rect.top + 1
-
-
+                if self.pos.y < hits[0].rect.bottom:
+                    if hits[0].point == True:
+                        hits[0].point = False
+                        self.score += 1
+                    self.vel.y = 0
+                    self.pos.y = hits[0].rect.top + 1
+                    self.jumping = False
 
 
 class platform(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, width = 0, height = 18):
         super().__init__()
-        self.surf = pygame.Surface((random.randint(50,100), 12))
-        self.surf.fill((0, 255, 0))
+        if width == 0: width = random.randint(50, 120)
+
+        self.image = pygame.image.load("platform.png")
+        self.surf = pygame.transform.scale(self.image, (width, height))
         self.rect = self.surf.get_rect(center = (random.randint(0, WIDTH-10),
                                                  random.randint(0, HEIGHT-30)))
+        self.speed = random.randint(-1, 1)
+        self.moving = True
+        self.point = True
     def move(self):
-        pass
+        hits = self.rect.colliderect(P1.rect)
+        if self.moving == True:
+            self.rect.move_ip(self.speed, 0)
+            if hits:
+                P1.pos += (self.speed, 0)
+            if self.speed > 0 and self.rect.left > WIDTH:
+                self.rect.right = 0
+            if self.speed < 0 and self.rect.right < 0:
+                self.rect.left = WIDTH
+    def generateCoin(self):
+        if (self.speed == 0):
+            coins.add(Coin((self.rect.centerx, self.rect.centery - 50)))
+
+class Coin(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__()
+        
+        self.image = pygame.image.load("Coin.png")
+        self.rect = self.image.get_rect()
+        self.rect.topleft = pos
+    def update(self):
+        if self.rect.colliderect(P1.rect):
+            P1.score += 5
+            self.kill()
+def check(platform, groupies):
+    if pygame.sprite.spritecollideany(platform, groupies):
+        return True
+    else:
+        for entity in groupies:
+            if entity == platform:
+                continue
+            if (abs(platform.rect.top - entity.rect.bottom) <40) and (abs(platform.rect.bottom - entity.rect.top) < 40):
+                return True
+            C = False
+
+def plat_gen():
+    while len(platforms) < 6:
+        width = random.randrange(50,100)
+        
+        C = True
+        while C:
+            p = platform()
+            p.rect.center = (random.randrange(0, WIDTH - width),
+                            random.randrange(-50, 0))
+            C = check(p, platforms)
+        p.generateCoin()
+        platforms.add(p)
+        all_sprites.add(p)
 
 
 PT1 = platform()
 PT1.surf = pygame.Surface((WIDTH, 20))
 PT1.surf.fill((255, 0, 0))
 PT1.rect = PT1.surf.get_rect(center = (WIDTH/2, HEIGHT -10))
+PT1.moving = False
+PT1.point = False
 P1 = Player()
 
 all_sprites = pygame.sprite.Group()
@@ -89,20 +154,19 @@ all_sprites.add(P1)
 platforms = pygame.sprite.Group()
 platforms.add(PT1)
 
-def plat_gen():
-    while len(platforms) < 7:
-        width = random.randrange(50,100)
-        p = platform()
-        p.rect.center = (random.randrange(0, WIDTH - width),
-                         random.randrange(-50, 0))
-        platforms.add(p)
-        all_sprites.add(p)
+coins = pygame.sprite.Group()
 
-for x in range(random.randint(5,6)):
+for x in range(random.randint(4,5)):
+    C = True
     pl = platform()
+    while C:
+        pl = platform()
+        C = check(pl, platforms)
+    pl.generateCoin()
     platforms.add(pl)
     all_sprites.add(pl)
 
+background = pygame.image.load("background.png")
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -111,22 +175,42 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 P1.jump()
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE:
+                P1.cancel_jump()
     
-    displaysurface.fill((0,0,0))
+    displaysurface.fill((0, 0, 0))
+    displaysurface.blit(background, (0,0))
+
+    f = pygame.font.SysFont("Verdana", 20)
+    g = f.render(str(P1.score), True, (123,255,0))
+    displaysurface.blit(g, (WIDTH/2, 10))
     P1.update()
     
     plat_gen()
-    
+    if P1.rect.top > HEIGHT:
+        for entity in all_sprites:
+            entity.kill()
+            time.sleep(1)
+            displaysurface.fill(255,0,0)
+            pygame.display.update()
+            time.sleep(1)
+            pygame.quit()
+            sys.exit()
     if P1.rect.top <= HEIGHT / 3:
         P1.pos.y += abs(P1.vel.y)
         for plat in platforms:
             plat.rect.y += abs(P1.vel.y)
             if plat.rect.top >= HEIGHT:
                 plat.kill()
-    
+    for coin in coins:
+        displaysurface.blit(coin.image, coin.rect)
+        coin.update()
+
     for entity in all_sprites:
         displaysurface.blit(entity.surf, entity.rect)
         entity.move()
     
     pygame.display.update()
     FramePerSec.tick(FPS)
+
