8bd7b6e4fe3dd51988d4a48573d55654d1cafb7e
Mike Pineau <michael.pineau89@gmail.com>
2021-10-12 21:58:25 -0400

    added movement animations

--- games/src/games/rpg_platformer.py
+++ games/src/games/rpg_platformer.py
@@ -15,14 +15,14 @@ FRIC = -0.10
 FPS = 60
 FPS_CLOCK = pygame.time.Clock()
 COUNT = 0
-
+ASSETS = 'assets/rpg_platformer/'
 displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Game")
 
 class Background(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.bgimage = pygame.image.load("assets/rpg_platformer/Background.png")
+        self.bgimage = pygame.image.load(f"{ASSETS}Background.png")
         self.bgY = 0
         self.bgX = 0
     def render(self):
@@ -31,13 +31,21 @@ class Background(pygame.sprite.Sprite):
 class Ground(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("assets/rpg_platformer/Ground.png")
+        self.image = pygame.image.load(f"{ASSETS}Ground.png")
         self.rect = self.image.get_rect(center = (350, 350))
 
     def render(self):
         displaysurface.blit(self.image, (self.rect.x, self.rect.y))
 
+run_ani_R = [pygame.image.load(f"{ASSETS}Player_Sprite_R.png"), pygame.image.load(f"{ASSETS}Player_Sprite2_R.png"),
+             pygame.image.load(f"{ASSETS}Player_Sprite3_R.png"), pygame.image.load(f"{ASSETS}Player_Sprite4_R.png"),
+             pygame.image.load(f"{ASSETS}Player_Sprite5_R.png"), pygame.image.load(f"{ASSETS}Player_Sprite6_R.png"),
+             pygame.image.load(f"{ASSETS}Player_Sprite_R.png")]
 
+run_ani_L = [pygame.image.load(f"{ASSETS}Player_Sprite_L.png"), pygame.image.load(f"{ASSETS}Player_Sprite2_L.png"),
+             pygame.image.load(f"{ASSETS}Player_Sprite3_L.png"), pygame.image.load(f"{ASSETS}Player_Sprite4_L.png"),
+             pygame.image.load(f"{ASSETS}Player_Sprite5_L.png"), pygame.image.load(f"{ASSETS}Player_Sprite6_L.png"),
+             pygame.image.load(f"{ASSETS}Player_Sprite_L.png")]
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -49,7 +57,11 @@ class Player(pygame.sprite.Sprite):
         self.vel = vec(0,0)
         self.acc = vec(0, 0)
         self.direction = "RIGHT"
+        self.jumping = False
+        self.move_frame = 0
+
     def move(self):
+        self.acc = vec(0, 0.5)
         if abs(self.vel.x) > 0.3:
             self.running = True
         else:
@@ -72,12 +84,46 @@ class Player(pygame.sprite.Sprite):
             self.pos.x = 0
         
         self.rect.midbottom = self.pos
+    def gravity_check(self):
+        hits = pygame.sprite.spritecollide(player, ground_group, False)
+        if self.vel.y > 0:
+            if hits:
+                lowest = hits[0]
+                if self.pos.y < lowest.rect.bottom:
+                    self.pos.y = lowest.rect.top + 1
+                    self.vel.y = 0
+                    self.jumping = False
     def update(self):
-        pass
+        if self.move_frame > 6:
+            self.move_frame = 0
+            return
+        if self.jumping == False and self.running == True:
+            if self.vel.x > 0:
+                self.image = run_ani_R[self.move_frame]
+                self.direction = "RIGHT"
+            elif self.vel.y < 0:
+                self.image = run_ani_L[self.move_frame]
+                self.direction = "LEFT"
+            self.move_frame += 1
+        
+        if abs(self.vel.x) < 0.2 and self.move_frame != 0:
+            self.move_frame = 0
+            if self.direction == "RIGHT":
+                self.image = run_ani_R[self.move_frame]
+            elif self.direction == "LEFT":
+                self.image = run_ani_L[self.move_frame]
     def attack(self):
         pass
     def jump(self):
-        pass
+        self.rect.x += 1
+        hits = pygame.sprite.spritecollide(self, ground_group, False)
+        self.rect.x -= 1
+
+        if hits and not self.jumping:
+            self.jumping = True
+            self.vel.y = -12
+
+
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
@@ -87,7 +133,15 @@ background = Background()
 ground = Ground()
 
 player = Player()
+
+all_sprites = pygame.sprite.Group()
+ground_group = pygame.sprite.Group()
+
+ground_group.add(ground)
+
 while True:
+    player.gravity_check()
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -96,7 +150,8 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN:
             pass
         if event.type == pygame.KEYDOWN:
-            pass
+            if event.key == pygame.K_SPACE:
+                player.jump()
     background.render()
     ground.render()
     player.move()
