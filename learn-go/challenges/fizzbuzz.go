79af84909dc733e599b58c6768cdcf04db3c1194
Mike <michael.pineau89@gmail.com>
2024-12-17 16:45:19 +0000

    update readme
    updated readme file with a little more information

---
+++ learn-go/challenges/fizzbuzz.go
@@ -0,0 +1,18 @@
+package fizzbuzz
+
+import (
+	"fmt"
+)
+
+func fizzbuzz() {
+
+	for a := 1; a < 20; a++ {
+		if a%5 == 0 && a%3 == 0 {
+			fmt.Println("fizzbuzz")
+		} else if a%3 == 0 {
+			fmt.Println("fizz")
+		} else if a%5 == 0 {
+			fmt.Println("buzz")
+		}
+	}
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# MiniProjects
+
+Workfolder where I place miscelaneous projects that don't have any perticular application. Feel free to copy from whatever you'd like. It generally just holds learning material.
